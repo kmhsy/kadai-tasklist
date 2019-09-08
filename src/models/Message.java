1a9f7f9f8d639758c01e0104267c7a71aaf57c49
ku_ni_ <kmhsy924437@gmail.com>
2019-09-08 14:19:17 +0900

    Add database files

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,5 +1,4 @@
 package models;
-
 import java.sql.Timestamp;
 
 import javax.persistence.Column;
@@ -7,28 +6,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@NamedQueries({
-    @NamedQuery(
-            name = "getAllMessages",
-            query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
-            )
-})
-
-@Table(name = "messages")
-
+@Table(name ="tasks")
 public class Message {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "title", length = 255, nullable = false)
-    private String title;
+
+
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -47,13 +36,8 @@ public class Message {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+
 
     public String getContent() {
         return content;
@@ -79,5 +63,4 @@ public class Message {
         this.updated_at = updated_at;
     }
 }
-
 
